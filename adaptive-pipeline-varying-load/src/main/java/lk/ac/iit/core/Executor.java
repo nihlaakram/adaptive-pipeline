801db7145722886a6eca7daeae689007faa086ce
nihlaakram <nihlaakram@yahoo.com>
2018-01-19 23:50:14 +0530

    implementing executor

--- adaptive-pipeline-varying-load/src/main/java/lk/ac/iit/core/Executor.java
+++ adaptive-pipeline-varying-load/src/main/java/lk/ac/iit/core/Executor.java
@@ -1,4 +1,37 @@
 package lk.ac.iit.core;
 
-public class Executor {
+public class Executor implements Runnable{
+
+    private Planner planner;
+    private static Executor executor;
+
+    private boolean terminate;
+
+    private Executor(){
+        this.planner = new Planner();
+        this.terminate = false;
+    }
+
+    public synchronized static Executor getExecutor(){
+        if(executor == null){
+            executor = new Executor();
+        }
+        return executor;
+    }
+
+    @Override
+    public void run() {
+        //runs until application terminates
+        while ( ! this.terminate) {
+
+            //plan scaling
+
+            //execute scaling
+
+        }
+    }
+
+    public void setTerminate(){
+        this.terminate = true;
+    }
 }
