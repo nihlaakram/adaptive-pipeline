62bdee1311a3a9b08aee6b0a35f29243589e967e
nihlaakram <nihlaakram@yahoo.com>
2018-01-20 00:40:50 +0530

    testing monitor initialization

--- adaptive-pipeline-varying-load/src/main/java/lk/ac/iit/core/Monitor.java
+++ adaptive-pipeline-varying-load/src/main/java/lk/ac/iit/core/Monitor.java
@@ -13,22 +13,34 @@ public class Monitor implements Runnable{
 
 
 
-    private Monitor(){
+    private Monitor(int monitorCount){
         this.analyzer = new Analyzer();
         this.terminated = false;
+        this.monitorCount = monitorCount;
     }
 
     public synchronized static Monitor getMonitor(){
-        if(monitor == null){
-            monitor = new Monitor();
-        }
+        System.out.println("Requested for monitor"+monitor);
         return monitor;
     }
 
     @Override
     public void run() {
 
-        while ()
+        while (!this.terminated){}
 
+    }
+
+    public void setTerminated(){
+        this.terminated = true;
+    }
+
+    public static void initMonitor(int monitorCount){
+        if(monitor == null){
+
+            monitor = new Monitor(monitorCount);
+            System.out.println("No monitor fount, creates one"+monitor);
+        }
+        //return monitor;
     }
 }
---
+++ adaptive-pipeline-varying-load/src/test/java/MonitorTest.java
@@ -0,0 +1,11 @@
+import lk.ac.iit.core.Monitor;
+
+public class MonitorTest {
+
+    public static void main(String[] args) {
+        Monitor.initMonitor(1000);
+
+        Monitor.getMonitor();
+        Monitor.getMonitor();
+    }
+}
