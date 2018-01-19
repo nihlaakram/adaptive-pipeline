19572b3f987bbd6cbcdb643a70561cd1b804f5c1
nihlaakram <nihlaakram@yahoo.com>
2018-01-19 23:40:27 +0530

    implementing singleton monitor

--- adaptive-pipeline-varying-load/src/main/java/lk/ac/iit/core/Monitor.java
+++ adaptive-pipeline-varying-load/src/main/java/lk/ac/iit/core/Monitor.java
@@ -1,4 +1,21 @@
 package lk.ac.iit.core;
 
+
+
+//
 public class Monitor {
+
+    private Analyzer analyzer;
+    private static Monitor monitor;
+
+    private Monitor(){
+        this.analyzer = new Analyzer();
+    }
+
+    public synchronized static Monitor getMonitor(){
+        if(monitor == null){
+            monitor = new Monitor();
+        }
+        return monitor;
+    }
 }
