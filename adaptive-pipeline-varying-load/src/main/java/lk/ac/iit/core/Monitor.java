dd169a5b96d3fefa97d29c03bbb029cc1d4268a2
nihlaakram <nihlaakram@yahoo.com>
2018-01-31 20:59:49 +0530

    implementing monitor operations

--- adaptive-pipeline-varying-load/src/main/java/lk/ac/iit/core/Monitor.java
+++ adaptive-pipeline-varying-load/src/main/java/lk/ac/iit/core/Monitor.java
@@ -1,15 +1,11 @@
 package lk.ac.iit.core;
 
 
-import lk.ac.iit.execption.NegativeTimestampException;
-
-import java.util.ArrayList;
-
 //
 public class Monitor implements Runnable{
 
     private Analyzer analyzer;
-    private static Monitor monitor;
+    private static Monitor monitor1;
 
     private int monitorThreshold;
     private int noOfStage;
@@ -30,31 +26,45 @@ public class Monitor implements Runnable{
 
     }
 
-    //access monitor functions through this
-    public synchronized static Monitor getMonitor(){
-        return monitor;
+    //access monitor1 functions through this
+    public synchronized static Monitor getMonitor1(){
+        return monitor1;
     }
 
     @Override
     public void run() {
 
         while (!this.terminated){
-            //if count is reached, send data to analyser
-            if(this.tempCount==this.monitorThreshold){
-                for(int i = 0; i<this.monitorThreshold; i++){
-                    //System.out.print(i+" ");
-                    for(int j = 0; j<this.noOfStage; j++){
-                        System.out.print(this.timeArray[i][j]+"\t");
-                    }
-                    System.out.println();
 
+                if(this.tempCount==this.monitorThreshold) {
+                    System.out.println("Temp Count : " + this.tempCount);
+                    sendDataToAnalyser();
+                    resetMonitor();
                 }
-                break;
-            }
+
+
+
 
 
         }
 
+    }
+
+    private synchronized void sendDataToAnalyser(){
+        for(int i = 0; i<this.monitorThreshold; i++){
+            //System.out.print(i+" ");
+            for(int j = 0; j<this.noOfStage; j++){
+                System.out.print(this.timeArray[i][j]+"\t");
+            }
+            System.out.println();
+
+        }
+    }
+
+    private synchronized void resetMonitor(){
+        this.timeArray = new long[monitorThreshold][noOfStage];
+        this.tempCount = 0;
+        notifyAll();
     }
 
     public void setTerminated(){
@@ -62,14 +72,14 @@ public class Monitor implements Runnable{
     }
 
     public static void initMonitor(Analyzer analyzer, int stageCount, int monitorCount){
-        if(monitor == null){
+        if(monitor1 == null){
             //lazy thread safe
-            monitor = new Monitor(analyzer, stageCount, monitorCount);
+            monitor1 = new Monitor(analyzer, stageCount, monitorCount);
         }
     }
 
     //receive timestamp related data
-    public void setTimestamp(long [] timestamp) throws NegativeTimestampException{
+    public synchronized void setTimestamp(long [] timestamp) {
 
 //            for(int i = 0; i<this.noOfStage; i++){
 //                if(timestamp[i]<0l){
@@ -79,8 +89,17 @@ public class Monitor implements Runnable{
 //                }
 //
 //            }
+        if(tempCount==1000){
+
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         this.timeArray[this.tempCount] = timestamp;
         this.tempCount++;
+
 
 
     }
--- adaptive-pipeline-varying-load/src/test/java/MonitorTest.java
+++ adaptive-pipeline-varying-load/src/test/java/MonitorTest.java
@@ -1,21 +1,18 @@
 import lk.ac.iit.core.Monitor;
-import lk.ac.iit.execption.NegativeTimestampException;
 
 public class MonitorTest {
 
     public static void main(String[] args) {
         Monitor.initMonitor(null, 3, 1000);
 
-        //Monitor.getMonitor();
-        Monitor monitor = Monitor.getMonitor();
-        Thread t1 = new Thread(monitor);
+        //Monitor.getMonitor1();
+        Monitor monitor1 = Monitor.getMonitor1();
+        Thread t1 = new Thread(monitor1);
         t1.start();
-        for (int i =0; i<1000; i++){
-            try {
-                monitor.setTimestamp(new long[]{i,i+1,i+2});
-            } catch (NegativeTimestampException e) {
-                e.printStackTrace();
-            }
+        for (int i =0; i<2000; i++){
+
+                monitor1.setTimestamp(new long[]{i,i+1,i+2});
+
 
         }
         System.out.println("completed sending data");
