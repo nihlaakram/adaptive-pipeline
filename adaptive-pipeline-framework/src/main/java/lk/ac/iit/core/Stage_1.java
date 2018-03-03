59de421fdbffea73e9c188fefdb69b32a56e052d
nihlaakram <nihlaakram@yahoo.com>
2018-03-04 00:46:28 +0530

    renaming module

---
+++ adaptive-pipeline-framework/src/main/java/lk/ac/iit/core/Stage_1.java
@@ -0,0 +1,51 @@
+package lk.ac.iit.core;
+
+import com.lmax.disruptor.EventHandler;
+import lk.ac.iit.data.LongEvent;
+
+public class Stage_1 implements EventHandler<LongEvent> {
+    private static int num;
+    private static int count = 0;
+    private static long starttime;
+    private static long totallatency = 0;
+    private final int id;
+    private String name;
+
+
+    public Stage_1(String name, int id, int num) {
+        this.name = name;
+        this.id = id;
+        this.num = num;
+        this.count = 0;
+        this.totallatency = 0;
+        starttime = System.currentTimeMillis();
+    }
+
+    public synchronized static long getNum() {
+        return Stage_1.num;
+    }
+
+    public synchronized static void setNum(int num) {
+        Stage_1.num = num;
+    }
+
+    private synchronized static void incrementCount() {
+        count++;
+    }
+
+    public void onEvent(LongEvent event, long sequence, boolean endOfBatch) {
+
+        if (event.getId() % getNum() == this.id) {
+            //for(int i=0; i<10000; i++){}
+            incrementCount();
+            totallatency = (System.currentTimeMillis() - event.getTimestamp());
+
+            if (count % 100 == 0) {
+                System.out.println(count + "\tavg lat: " + ((totallatency * 1.0) / count));
+            }
+        }
+    }
+
+
+}
+
---
+++ adaptive-pipeline-framework/src/main/java/lk/ac/iit/main/Test.java
@@ -0,0 +1,22 @@
+package lk.ac.iit.main;
+
+public class Test {
+
+    public static void main(String[] args) {
+        //{{1,2},{3,4},{5,6}
+        int arr[][] = new int[2][3];
+        arr[0][0] = 1;
+        arr[0][1] = 2;
+        arr[0][2] = 3;
+        arr[1][0] = 4;
+        arr[1][1] = 5;
+        arr[1][2] = 6;
+
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
+    }
+}
