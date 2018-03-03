e9eed72a1fb03d2606799b406f34187d14e94836
nihlaakram <nihlaakram@yahoo.com>
2018-03-04 01:18:36 +0530

    code refactor

--- adaptive-pipeline-framework/src/main/java/lk/ac/iit/core/analyser/Analyser.java
+++ adaptive-pipeline-framework/src/main/java/lk/ac/iit/core/analyser/Analyser.java
@@ -1,37 +1,47 @@
 package lk.ac.iit.core.analyser;
 
 import lk.ac.iit.core.analyser.learner.SiddhiLearner;
+import lk.ac.iit.core.planner.Planner;
 import org.wso2.siddhi.core.event.Event;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Analyser {
 
     private SiddhiLearner siddhi;
-    private int noOfStages;
-    private int noOfSummarizer;
     private int monitorThreshold;
+    private int objLength;
+    private Planner planner;
 
-    public Analyser(int noOfStages, int monitorThreshold) {
-
-        this.noOfStages = noOfStages;
+    public Analyser(int noOfStages, int monitorThreshold, Planner planner, boolean isScale) {
         this.monitorThreshold = monitorThreshold;
-        this.noOfSummarizer = getNoOfStages();
-        this.siddhi = new SiddhiLearner(this.monitorThreshold);
-
-        //initPerfSummarizer();
-
-    }
-
-
-    public void analyse(long[] timestamp) {
-
-        siddhi.publish(new Event(System.currentTimeMillis(), new Object[]{timestamp[1] - timestamp[0], timestamp[2] - timestamp[1]}));
+        this.objLength = 2 * noOfStages;
+        this.planner = planner;
+        this.siddhi = new SiddhiLearner(this.monitorThreshold, this.objLength, this.planner, isScale);
 
     }
 
 
-    public int getNoOfStages() {
-        return this.noOfStages;
+    public void analyse(long... timestamp) {
+
+        Object[] latencyPreprocessed = new Object[this.objLength / 2];
+        Object[] tpsPreprocessed = new Object[this.objLength / 2];
+
+        //preprocessed attributes for latency and tps
+        for (int i = 0; i < this.objLength / 2; i++) {
+            latencyPreprocessed[i] = timestamp[i + 1] - timestamp[i];
+            tpsPreprocessed[i] = timestamp[i];
+        }
+        List list = new ArrayList(Arrays.asList(latencyPreprocessed));
+        list.addAll(Arrays.asList(tpsPreprocessed));
+        Object[] obj = list.toArray();
+
+        siddhi.publish(new Event(System.currentTimeMillis(), obj));
+
     }
+
 
 
 }
