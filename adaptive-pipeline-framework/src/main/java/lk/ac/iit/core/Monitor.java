7b6db80fd235c3c249afeaf938487002c3c53609
nihlaakram <nihlaakram@yahoo.com>
2018-03-04 01:04:25 +0530

    adding monitoring component

--- adaptive-pipeline-framework/src/main/java/lk/ac/iit/core/Monitor.java
+++ adaptive-pipeline-framework/src/main/java/lk/ac/iit/core/Monitor.java
@@ -4,97 +4,51 @@ package lk.ac.iit.core;
 import lk.ac.iit.core.analyser.Analyser;
 import lk.ac.iit.core.analyser.AnalyserData;
 
+import lk.ac.iit.core.analyser.Analyser;
+import lk.ac.iit.core.analyser.learner.SiddhiLearner;
+
 //
-public class Monitor implements Runnable {
+public class Monitor {
 
     private static Monitor monitor1;
     private Analyser analyser;
     private int monitorThreshold;
     private int noOfStage;
-    private boolean terminated;
 
-    private long[][] timeArray;
-    private int tempCount = 0;
+    private Executor executor;
 
 
-    public Monitor(Executor executor, int noOfStage, int monitorThreshold) {
+    public Monitor(int noOfStage, int monitorThreshold) {
         //StaticBlockSingleton
-        this.analyser = new Analyser(executor, noOfStage, monitorThreshold);
-        this.terminated = false;
+        this.analyser = new Analyser(noOfStage, monitorThreshold);
         this.monitorThreshold = monitorThreshold;
         this.noOfStage = noOfStage;
-        this.timeArray = new long[monitorThreshold][noOfStage];
+        this.executor = new Executor(noOfStage);
 
     }
 
     //access monitor1 functions through this
     public synchronized static Monitor getMonitor1() {
+
         return monitor1;
     }
 
-    public static void initMonitor(Executor executor, int stageCount, int monitorCount) {
+    public static void initMonitor(int stageCount, int monitorCount) {
         if (monitor1 == null) {
             //lazy thread safe
-            monitor1 = new Monitor(executor, stageCount, monitorCount);
+            monitor1 = new Monitor(stageCount, monitorCount);
         }
     }
 
-    @Override
-    public void run() {
-
-        while (!this.terminated) {
-
-            if (this.tempCount == this.monitorThreshold) {
-                System.out.println("Temp Count : " + this.tempCount);
-                sendDataToAnalyser();
-                resetMonitor();
-            }
-
-
-        }
-
-    }
-
-    private synchronized void sendDataToAnalyser() {
-        AnalyserData analyserData = this.analyser.analyse(timeArray);
-        //calculate
-        System.out.println("Avg Lat "+analyserData.getAvgLatency()[0]+"\t"+analyserData.getAvgLatency()[1]+"\t");
-        System.out.println("TPS "+analyserData.getTpsArr()[0]+"\t"+analyserData.getTpsArr()[1]+"\t");
-
-    }
-
-    private synchronized void resetMonitor() {
-        this.timeArray = new long[monitorThreshold][noOfStage];
-        this.tempCount = 0;
-        notifyAll();
-    }
-
-    public void setTerminated() {
-        this.terminated = true;
-    }
 
     //receive timestamp related data
     public synchronized void setTimestamp(long[] timestamp) {
 
-//            for(int i = 0; i<this.noOfStage; i++){
-//                if(timestamp[i]<0l){
-//                    throw new NegativeTimestampException(timestamp[i]);
-//                } else {
-//                    this.timeArray[i].add(timestamp[i]);
-//                }
-//
-//            }
-        if (tempCount == 1000) {
+        analyser.analyse(timestamp);
 
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        this.timeArray[this.tempCount] = timestamp;
-        this.tempCount++;
+    }
 
-
+    public Executor getExecutor() {
+        return this.executor;
     }
 }
