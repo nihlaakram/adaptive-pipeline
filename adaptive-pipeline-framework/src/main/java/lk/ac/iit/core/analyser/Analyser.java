35e01adca059326786b3cea248e0c5ebb7335ee3
nihlaakram <nihlaakram@yahoo.com>
2018-03-04 00:59:59 +0530

    adding analysis component

--- adaptive-pipeline-framework/src/main/java/lk/ac/iit/core/analyser/Analyser.java
+++ adaptive-pipeline-framework/src/main/java/lk/ac/iit/core/analyser/Analyser.java
@@ -1,73 +1,37 @@
 package lk.ac.iit.core.analyser;
 
-import lk.ac.iit.core.Executor;
+import lk.ac.iit.core.analyser.learner.SiddhiLearner;
+import org.wso2.siddhi.core.event.Event;
 
 public class Analyser {
 
+    private SiddhiLearner siddhi;
     private int noOfStages;
     private int noOfSummarizer;
     private int monitorThreshold;
-    private Executor executor;
 
-    private long[] latencyArr;
-    private double[] tpsArr;
-    private long [] startTime;
-    private long [] endTime;
-    private double [] avgLatency;
+    public Analyser(int noOfStages, int monitorThreshold) {
 
-    public Analyser(Executor executor, int noOfStages, int monitorThreshold) {
-        this.executor = executor;
         this.noOfStages = noOfStages;
         this.monitorThreshold = monitorThreshold;
-        this.noOfSummarizer = getNoOfStages() -1;
+        this.noOfSummarizer = getNoOfStages();
+        this.siddhi = new SiddhiLearner(this.monitorThreshold);
 
         //initPerfSummarizer();
 
     }
 
-    private void initPerfSummarizer(){
-        this.latencyArr = new long [noOfSummarizer];
-        this.tpsArr = new double [noOfSummarizer];
-        this.startTime = new long [getNoOfStages()];
-        this.endTime = new long [getNoOfStages()];
-        this.avgLatency = new double[getNoOfStages()-1];
-    }
 
-    public AnalyserData analyse(long[][] timeData) {
-        initPerfSummarizer();
-        //startTime and endTime related data
-        this.startTime = timeData[0];
-        this.endTime = timeData[getMonitorThreshold()-1];
+    public void analyse(long[] timestamp) {
 
-        //calculate TPS & average latency
-        for (int j = 0; j < this.noOfStages-1; j++) {
-            for (int i = 0; i < this.monitorThreshold; i++) {
-                this.latencyArr[j] = latencyArr[j]+(timeData[i][j+1]-timeData[i][j]);
-
-            }
-            avgLatency[j] = (latencyArr[j]/getMonitorThreshold()) ;
-            tpsArr[j] = getMonitorThreshold()/(endTime[j+1]-startTime[j]);
-        }
-
-        AnalyserData analyzerData = new AnalyserData(this.tpsArr, this.avgLatency);
-        return analyzerData;
-
-
+        siddhi.publish(new Event(System.currentTimeMillis(), new Object[]{timestamp[1] - timestamp[0], timestamp[2] - timestamp[1]}));
 
     }
-
 
 
     public int getNoOfStages() {
         return this.noOfStages;
     }
 
-    public int getMonitorThreshold() {
-        return this.monitorThreshold;
-    }
-
-    public Executor getExecutor() {
-        return this.executor;
-    }
 
 }
