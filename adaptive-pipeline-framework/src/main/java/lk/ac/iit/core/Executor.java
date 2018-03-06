504733ede6c748437852cc2f3da130fc6c18cb38
nihlaakram <nihlaakram@yahoo.com>
2018-03-07 01:19:20 +0530

    code refactor

--- adaptive-pipeline-framework/src/main/java/lk/ac/iit/core/Executor.java
+++ adaptive-pipeline-framework/src/main/java/lk/ac/iit/core/Executor.java
@@ -2,11 +2,13 @@ package lk.ac.iit.core;
 
 import lk.ac.iit.stage.HandlerStage;
 import lk.ac.iit.stage.ProducerStage;
+import lk.ac.iit.stage.TerminationStage;
 
 public class Executor {
 
     private HandlerStage[] handlerStages;
     private ProducerStage producerStage;
+    private TerminationStage terminationStage;
 
     public Executor(int noOfHandlers) {
         handlerStages = new HandlerStage[noOfHandlers];
@@ -17,9 +19,14 @@ public class Executor {
     }
 
     public void executeScaling(int stageID) {
-        HandlerStage handlerStage = this.handlerStages[stageID - 1].clone();
-        Thread t1 = new Thread(handlerStage);
-        t1.start();
+
+
+            HandlerStage handlerStage = this.handlerStages[stageID - 1].clone();
+            Thread t1 = new Thread(handlerStage);
+            t1.start();
+            System.out.println("Scaling Stage :"+stageID);
+
+
 
     }
 
@@ -32,10 +39,18 @@ public class Executor {
         //Start producer
         this.producerStage.start();
 
+        //startTerminator
+        Thread term = new Thread(this.terminationStage, "termination-thread");
+        term.start();
+
         //Start the handlers
         for(int i=0; i<this.handlerStages.length; i++){
             Thread thread = new Thread(this.handlerStages[i], "handler-thread-"+(i+1));
             thread.start();
         }
+    }
+
+    public void addTerminator(TerminationStage term) {
+        this.terminationStage = term;
     }
 }
