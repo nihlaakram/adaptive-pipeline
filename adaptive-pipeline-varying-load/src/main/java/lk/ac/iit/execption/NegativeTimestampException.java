5c0ab4260f3778f45d10d044633c9291c49da02a
nihlaakram <nihlaakram@yahoo.com>
2018-03-07 01:18:47 +0530

    code refactor

--- adaptive-pipeline-varying-load/src/main/java/lk/ac/iit/execption/NegativeTimestampException.java
+++
@@ -1,8 +0,0 @@
-package lk.ac.iit.execption;
-
-public class NegativeTimestampException extends Exception {
-
-    public NegativeTimestampException(long time) {
-        super(String.valueOf(time));
-    }
-}
