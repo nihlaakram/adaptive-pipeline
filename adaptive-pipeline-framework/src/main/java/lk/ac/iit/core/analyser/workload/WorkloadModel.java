66503b7c227c84480be885ce205b13dc14900247
nihlaakram <nihlaakram@yahoo.com>
2018-03-10 00:53:54 +0530

    adding workload related data

--- adaptive-pipeline-framework/src/main/java/lk/ac/iit/core/analyser/workload/WorkloadModel.java
+++ adaptive-pipeline-framework/src/main/java/lk/ac/iit/core/analyser/workload/WorkloadModel.java
@@ -1,15 +1,25 @@
 package lk.ac.iit.core.analyser.workload;
 
-public class WorkloadModel {
+public class WorkloadModel implements BaseModel{
 
-    private double [][] pastResults;
+    private double[][] pastResults;
 
     /**
      * Creates a WorkLoadModel with past data
-     * --
-     * @param noOfResults the no of workload ewsults that will be added to the model
+     *
+     * @param noOfResults the no of workload results that will be added to the model
      */
-   public WorkloadModel(int noOfResults) {
+    public WorkloadModel(int noOfResults) {
+        this.pastResults = new double[3][noOfResults];
+    }
 
-   }
+    @Override
+    public double getWorkers(double inputRate, double workLoadParam) {
+        return 0;
+    }
+
+    @Override
+    public void setModel(BaseModel baseModel) {
+
+    }
 }
--- adaptive-pipeline-framework/src/main/java/lk/ac/iit/usecase/usecase01/Main.java
+++ adaptive-pipeline-framework/src/main/java/lk/ac/iit/usecase/usecase01/Main.java
@@ -1,7 +1,10 @@
 package lk.ac.iit.usecase.usecase01;
 
+import lk.ac.iit.core.analyser.workload.WorkloadModel;
+
 public class Main {
     public static void main(String[] args) {
 
+        WorkloadModel model = new WorkloadModel(7);
     }
 }
