1fca28d7b2db7a0d68fbf1ba2d3229f80d4d6463
Guest18 <nihlaakram@yahoo.com>
2018-05-29 03:05:53 +0530

    code refactor

--- adaptive-pipeline-framework/src/main/java/lk/ac/iit/visual/Test.java
+++ adaptive-pipeline-framework/src/main/java/lk/ac/iit/visual/Test.java
@@ -25,7 +25,6 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 import lk.ac.iit.core.analyser.data.AnalyserReport;
 import lk.ac.iit.visual.data.ExternalReader;
-import lk.ac.iit.visual.data.PerformanceInstance;
 import lk.ac.iit.visual.data.PerformanceLogger;
 
 public class Test extends Application {
